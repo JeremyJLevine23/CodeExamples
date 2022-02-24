b805aea62b9e7dac37c3c0d7b2dec9b1087b8914
JeremyJLevine23 <100331641+JeremyJLevine23@users.noreply.github.com>
2022-02-23 21:54:47 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,171 +1,212 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace MessageDecoder
+namespace ZumbaGym
 {
     class Program
     {
-        /*
-        public static char cipher(char ch, int key)
+        public class Zumba
         {
-            if (!char.IsLetter(ch))
+            // initizalize data
+            // weekly attendance in 2D array
+            int[,] weeklyClassAttendance;
+            int totalClassAttendance;
+            int classPrice = 4;
+            // daily attendance in 1D array
+            int[] dailyClassAttendance;
+            // daily revenue in 1D array
+            int[] dailyClassRevenue;
+            // weekly time slot attendance in 1D array
+            int[] classAttendance;
+            // weekly time slot revenue in 1D array
+            int[] weeklyClassRevenue;
+            // per time-slot revenue in 1D array
+            int[] perTimeSlotRevenue;
+
+            // methods
+            public Zumba(int[,] attendance)
             {
-                return ch;
+                weeklyClassAttendance = attendance;
+                int row = attendance.GetLength(0);
+                int col = attendance.GetLength(1);
+                dailyClassAttendance = new int[row];
+                classAttendance = new int[col];
+                dailyClassRevenue = new int[row];
+                perTimeSlotRevenue = new int[col];
             }
-            char d = char.IsUpper(ch) ? 'A' : 'a';
-            return (char)((((ch + key) - d) % 26) + d);
-        }
 
-
-        public static string Encipher(string input, int key)
-        {
-            string output = string.Empty;
-            foreach (char ch in input)
+            //display method for Zumba class information
+            public void DisplayZumba()
             {
-                output += cipher(ch, key);
-            }
-            return output;
-        }
-
-        public static string Decipher(string input, int key)
-        {
-            return Encipher(input, 26 - key);
-        }
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Type a string to encrypt:");
-            string UserString = Console.ReadLine();
-
-            Console.WriteLine("\n");
-
-            Console.Write("Enter your Key");
-            int key = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n");
-
-            Console.WriteLine("Encrypted Data");
-
-            string cipherText = Encipher(UserString, key);
-            Console.WriteLine(cipherText);
-            Console.Write("\n");
-
-            Console.WriteLine("Decrypted Data:");
-
-            string t = Decipher(cipherText, key);
-            Console.WriteLine(t);
-            Console.Write("\n");
-
-
-
-
-            Console.ReadKey();
-
-        }
-        */
-        public class DecodingProcess
-        {
-            //this function will count occurrence fo each charcter in string and stores it in a dictionary
-            public Dictionary<char, int> getOccurranceOfChars(string msg)
-            {
-                Dictionary<char, int> dict = new Dictionary<char, int>();
-                for (int i = 0; i < msg.Length; i++)
+                //weekly attendance 
+                Console.WriteLine("The weekly attendance is {0}", GetWeeklyAttendance());
+                //weekly revenue
+                Console.WriteLine("The weekly revenue is {0:C2}", GetWeeklyRevenue());
+                //per session attendance
+                Console.WriteLine("The per session attendance is: ");
+                GetPerSlotAttendance();
+                //traversing through classAttendance to get data to display to user
+                foreach (int i in classAttendance)
                 {
-                    int count = 0;
-                    if (!dict.ContainsKey(msg[i])) // checks for unique character
+                    Console.Write("{0} \t", i);
+                }
+                //space
+                Console.WriteLine();
+                //per day zumba class attendance
+                Console.WriteLine("The per day zumba class attendance is: ");
+                GetPerDayAttendance();
+                //traversing through dailyClassAttendance to get data to display to user
+                foreach (int i in dailyClassAttendance)
+                {
+                    Console.Write("{0} \t", i);
+                }
+                //space
+                Console.WriteLine();
+                //daily zumba revenue
+                Console.WriteLine("The daily revenue is: ");
+                GetPerDayRevenue();
+                //traversing through dailyClassRevenue to get data to display to user
+                foreach (int i in dailyClassRevenue)
+                {
+                    Console.Write("{0:C2} \t", i);
+                }
+                //space
+                Console.WriteLine();
+                //per class revenue
+                Console.WriteLine("The per class revenue is: ");
+                GetPerSlotRevenue();
+                //traversing through perTimeSlotRevenue to get data to display to user
+                foreach (int i in perTimeSlotRevenue)
+                {
+                    Console.Write("{0:C2} \t", i);
+                }
+                //space
+                Console.WriteLine();
+
+            }
+
+            public void RunZumba()
+            {
+                // need to ask user for yes or no
+                // do-while loop
+                do
+                {
+                    //ask user if they want to see zumba class information
+                    Console.WriteLine("Do you want to see the zumba class information? (yes/no)");
+                    string userInput = Console.ReadLine();
+                    if (userInput.ToLower() == "yes")
                     {
-                        for (int j = i; j < msg.Length; j++)
-                        {
-                            if (msg[i] == msg[j])
-                                count++;
-                        }
-                        dict.Add(msg[i], count);
+                        DisplayZumba();
+                    }
+                    //ask user if they want to continue on
+                    Console.WriteLine("Do you want to continue on? (yes/no)");
+                    userInput = Console.ReadLine();
+                    if (userInput.ToLower() != "yes")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    }
+
+                }
+                while (true);
+            }
+
+            //weekly attendance method
+            public int GetWeeklyAttendance()
+            {
+                //traversing through the weeklyClassAttendance and adding it to a 1D totalClassAttendance array
+                foreach (int a in weeklyClassAttendance)
+                {
+                    totalClassAttendance += a;
+                }
+
+                return totalClassAttendance;
+            }
+
+            //per slot attendance method
+            public int[] GetPerSlotAttendance()
+            {
+                //running through the rows and columns in weekly attendance to add total class attednace total
+                for (int k = 0; k < weeklyClassAttendance.GetLength(1); k++)
+                {
+                    for (int l = 0; l < weeklyClassAttendance.GetLength(0); l++)
+                    {
+                        //to get sum of columns for per time slot attendance
+                        classAttendance[k] += weeklyClassAttendance[l, k];
                     }
                 }
-                return dict;
+
+                return classAttendance;
             }
 
-            //returns character that is occurred max time in dictionary or in msg
-            public char getMaxOccurredChar(Dictionary<char, int> dict)
+            //get per day attendance method
+            public int[] GetPerDayAttendance()
             {
-                char c = 'a';
-                int max = 0;
-                foreach (KeyValuePair<char, int> pair in dict)
+                //running through the rows and columns to get the total per day attendance 
+                for (int j = 0; j < weeklyClassAttendance.GetLength(0); j++)
                 {
-                    if (pair.Value > max)
+                    for (int t = 0; t < weeklyClassAttendance.GetLength(1); t++)
                     {
-                        max = pair.Value;
-                        c = pair.Key;
+                        //sum of rows to get total daily attendance
+                        dailyClassAttendance[j] += weeklyClassAttendance[j, t];
                     }
                 }
-                Console.WriteLine("char " + c + " occurred maximum for " + max + " times");
-                return c;
+
+                return dailyClassAttendance;
             }
 
-            //computes key/shift based on max occurred character is E
-            public Dictionary<char, char> ComputeKeyValue(int start)
+            //weekly revenue method
+            public double GetWeeklyRevenue()
             {
-                Dictionary<char, char> dec = new Dictionary<char, char>();
-                for (int i = 0; i < 26; i++)
+                //weekly revenue calculation
+                return totalClassAttendance * classPrice;
+            }
+
+            //per time slot revenue method
+            public int[] GetPerSlotRevenue()
+            {
+                //running through the rows and columns to get the total per time slot revenue
+                for (int a = 0; a < weeklyClassAttendance.GetLength(1); a++)
                 {
-                    if (start > 90)
+                    for (int b = 0; b < weeklyClassAttendance.GetLength(0); b++)
                     {
-                        start = 65;
-                    } 
-                    dec.Add((char)start, (char)(i + 65));
-                    start++;
+                        //adding up the columns to get the total time slot revenue
+                        perTimeSlotRevenue[a] += weeklyClassAttendance[b, a] * classPrice;
+                    }
                 }
-                return dec;
+                return perTimeSlotRevenue;
             }
 
-            //decodes msg based on the decoding key
-            public void decodeMsg(string msg, char[] dmsg, Dictionary<char, char> dec)
+            //per day revenue method
+            public int[] GetPerDayRevenue()
             {
-                for (int i = 0; i < msg.Length; i++)
+                //running through the rows and colums to get the daily revenue
+                for (int i = 0; i < weeklyClassAttendance.GetLength(0); i++)
                 {
-                    dmsg[i] = dec[msg[i]];
-                    Console.Write(dmsg[i]);
+                    for (int j = 0; j < weeklyClassAttendance.GetLength(1); j++)
+                    {
+                        //adding up the rows to get the total daily revenue
+                        dailyClassRevenue[i] += weeklyClassAttendance[i, j] * classPrice;
+                    }
                 }
+                return dailyClassRevenue;
             }
-
-            //computes value for A
-            public int getStartValue(char c)
-            { 
-                int s = 0, d = 0;
-                if (c > 'E')
-                {
-                    d = c - 'E';
-                    s = c - 4;
-                }
-                else
-                {
-                    d = 'E' - c;
-                    s = c + 4;
-                }
-
-                Console.WriteLine("ASCII value difference is " + d);
-                Console.WriteLine();
-                return s;
-            }
-
-            public static void Main()
-
+        //main method
+            static void Main(string[] args)
             {
+                //creating zumba array with zumba class methods
+                Zumba zumba = new Zumba(
+                    new int[,]
+                    {
+                        {8,10,15,20 },
+                        {11,15,17,18 },
+                        {14,12,22,20 },
+                        {9,14,17,12 },
+                        {10,12,21,22 },
+                        {12,12,7,5 },
 
-                DecodingProcess decode = new DecodingProcess();
-                Console.WriteLine("What is the message you want to decode?: ");
-                String message = Console.ReadLine();
-                Console.WriteLine();
-                Dictionary<char, int> dict = decode.getOccurranceOfChars(message);
-                Console.WriteLine("Your Encoded Message: ");
-                Console.WriteLine(message);
-                Console.WriteLine();
-                char c = decode.getMaxOccurredChar(dict);
-                int start = decode.getStartValue(c);
-                Dictionary<char, char> dec = decode.ComputeKeyValue(start);
-                char[] decodedmessage = new char[message.Length];
-                Console.WriteLine("Your Decoded Message: ");
-                decode.decodeMsg(message, decodedmessage, dec);
+                    });
+                //calling the RunZumba() method
+                zumba.RunZumba();
             }
         }
     }
